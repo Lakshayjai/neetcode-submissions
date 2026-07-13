d9a4f6d68dce590c55530912af99c973b7e79a39
Lakshay Jain <134166057+Lakshayjai@users.noreply.github.com>
2026-07-13 13:17:41 +0530

    Bulk sync: 6 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-0.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        int n = s.length();
+        int m = t.length();
+
+        vector<int> arr1(26,0);
+        vector<int> arr2(26,0);
+
+        for(int i = 0; i < n; i++){
+            arr1[s[i] - 'a']++;
+        }
+
+        for(int i = 0; i < m; i++){
+            arr2[t[i] - 'a']++;
+        }
+
+        for(int i = 0; i < 26; i++){
+            if(arr1[i] != arr2[i]) return false; 
+        }
+
+        return true;
+    }
+};
