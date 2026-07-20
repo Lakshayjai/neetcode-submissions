4f4f44e6ae2e9a3f6a3709b245e165402e2fa2b3
Lakshay Jain <134166057+Lakshayjai@users.noreply.github.com>
2026-07-20 22:52:13 +0530

    Add: trapping-rain-water - submission-0

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-0.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n = height.size();
+
+        vector<int> left(n, 0);
+        left[0] = height[0];
+        vector<int> right(n, 0);
+        right[n-1] = height[n-1];
+
+        int sum = 0;
+
+        for(int i = 1; i < n; i++){
+            left[i] = max(height[i], left[i-1]);
+            right[n-i-1] = max(height[n-i-1], right[n-i]);
+        }
+
+        for(int i = 0; i < n; i++){
+            int val = min(left[i], right[i]) - height[i];
+            sum += val;
+        }
+
+        return sum;
+    }
+};
